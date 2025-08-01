5f81d315536ff20e36c0339132409d6edacb2451
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-01 14:53:25 -0300

    update: BoardColumns List

--- app/src/main/java/org/example/persistence/entity/BoardEntity.java
+++ app/src/main/java/org/example/persistence/entity/BoardEntity.java
@@ -1,9 +1,13 @@
 package org.example.persistence.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class BoardEntity {
     private Long id;
     private String name;
+    private List<BoardColumnEntity> boardColumns = new ArrayList<>();
 }
