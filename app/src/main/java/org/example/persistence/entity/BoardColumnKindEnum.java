2725bc0ff73fa567c5335278706f62375c0aff9c
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-01 15:23:39 -0300

    update: findByName method to KindEnum

--- app/src/main/java/org/example/persistence/entity/BoardColumnKindEnum.java
+++ app/src/main/java/org/example/persistence/entity/BoardColumnKindEnum.java
@@ -1,8 +1,16 @@
 package org.example.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnKindEnum {
     INITIAL,
     FINAL,
     CANCEL,
-    PENDING
+    PENDING;
+
+    public static BoardColumnKindEnum findByName(final String name){
+        return Stream.of(BoardColumnKindEnum.values())
+                .filter(val -> val.name().equals(name))
+                .findFirst().orElseThrow();
+    }
 }
